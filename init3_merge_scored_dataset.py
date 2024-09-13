5098228e22d1dd39d9bfcd81d4e08445be5b216b
肥鸡 <jiyuwen.jyw@alibaba-inc.com>
2024-09-13 12:24:52 +0800

    update

--- init3_merge_scored_dataset.py
+++ init3_merge_scored_dataset.py
@@ -13,9 +13,6 @@ def parse_args():
     parser.add_argument('--exps_dir', 
                         type=str, help='Directory for saving all the intermediate and final outputs.', 
                         required=True)
-    parser.add_argument('--num_chunks', 
-                        type=int, help='Number of chunks for Parallelism Scoring', 
-                        required=True)
     args = parser.parse_args()
     args.config_file = str(Path(args.exps_dir)/args.task_name/'config.json')
     args.taskpath = str(Path(args.exps_dir)/args.task_name)
@@ -26,7 +23,7 @@ def main():
     cfgpath = args.config_file
     config = getConfig(cfgpath,args)
     for inputfile in config.output_unscored_files:
-        name = f'*{Path(inputfile).stem}_{config.num_chunks}*.jsonl'
+        name = f'*{Path(inputfile).stem}*.jsonl'
         outputpath = Path(args.taskpath)/"scored"
         data = []
         for p in outputpath.rglob(name):
